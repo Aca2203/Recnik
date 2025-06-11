efd7251824c3dd5983fe56aab5b27d29d3d99881
Александар Стефановић <stefanovicsalex@gmail.com>
2025-06-11 15:08:29 +0200

    Приказивање целог значења речи у табели

--- src/gui/Forma.java
+++ src/gui/Forma.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 
 import imp.*;
 
@@ -69,6 +70,7 @@ public class Forma extends JFrame {
 		
 		model = new DefaultTableModel(new Object[]{"Реч", "Значење"}, 0);
         tabela = new JTable(model);
+        tabela.getColumnModel().getColumn(1).setCellRenderer(new TextAreaRenderer());
         JScrollPane scrollPane = new JScrollPane(tabela);
         panel1.add(scrollPane);
         
@@ -233,4 +235,28 @@ public class Forma extends JFrame {
 		new Forma();
 	}
 
+	static class TextAreaRenderer extends JTextArea implements TableCellRenderer {
+        public TextAreaRenderer() {
+            setLineWrap(true);
+            setWrapStyleWord(true);
+            setOpaque(true);
+        }
+
+        public Component getTableCellRendererComponent(JTable table, Object value,
+                boolean isSelected, boolean hasFocus, int row, int column) {
+            setText(value != null ? value.toString() : "");
+            setSize(table.getColumnModel().getColumn(column).getWidth(), getPreferredSize().height);
+            if (table.getRowHeight(row) != getPreferredSize().height) {
+                table.setRowHeight(row, getPreferredSize().height);
+            }
+            if (isSelected) {
+                setBackground(table.getSelectionBackground());
+                setForeground(table.getSelectionForeground());
+            } else {
+                setBackground(table.getBackground());
+                setForeground(table.getForeground());
+            }
+            return this;
+        }
+    }
 }
